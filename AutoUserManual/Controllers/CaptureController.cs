36e167241af51fb69474da2e740330dd4d9e1137
Murali Gunde <murali.gunde@smarterp.com>
2025-03-21 15:32:57 +0530

    Hi,Manjunath

---
+++ AutoUserManual/Controllers/CaptureController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserManualNew.DTOs;
+
+namespace UserManualNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaptureController : ControllerBase
+    {
+        private readonly ScreenshotService _screenshotService;
+
+        public CaptureController(ScreenshotService screenshotService)
+        {
+            _screenshotService = screenshotService;
+        }
+
+        [HttpPost("start")]
+        public async Task<ActionResult<ScreenshotDetailsDTO>> StartCapture()
+        {
+            var result = await _screenshotService.CaptureScreenshot();
+            return Ok(result);
+        }
+    }
+}
+
